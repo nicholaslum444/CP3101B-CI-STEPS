ffa47a04515dc7a0c14923c97f76e2a96851001d
sharmine <slxmlow@gmail.com>
2015-03-29 05:14:33 +0800

    Add editModulePage

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
-/*$(function() {
-	$( "#fakeLogin" ).click(function() {
-		var apiUrl = "https://ivle.nus.edu.sg/api/login/?apikey=3bBGOIdtC1T2d7SXeQAO9&url=";
-		var callbackUrl = "http://localhost:8080/index.php/ivleauth/";
-		openInNewWindow(apiUrl + callbackUrl);
-	});
-});
-
-function openInNewWindow(url) {
-	var newWindow = window.open(url,'name','height=500,width=700');
-	if (window.focus) {
-		newWindow.focus();
-		return newWindow;
-	}
-	return false;
-}
-*/
 function getLoginUrl(url) {
 	var loginUrl = "https://ivle.nus.edu.sg/api/login/?"
 					+ "apikey=3bBGOIdtC1T2d7SXeQAO9&url="
 					+ url + "index.php/IvleLogin";
 }
 
+$(function() {
+    $('.addProjectTitleBtn').click(function() {
+    	//Dynamically generate buttons
+    	$('.projectTitleFields').append('<input type="text" class="form-control inputField" placeholder="Project Title"style="display:block">');
+    });
+
+    $(".inputText").click(function() {
+    	//Turns from words to fields
+    	$(this).parent().closest('.field-group').children('.inputText').css("display", "none");
+    	$(this).parent().closest('.field-group').children('.glyphicon-pencil').css("display", "none");
+    	$(this).closest('.field-group').children('.inputField').css("display", "block");
+    	$(this).closest('.field-group').children('.glyphicon-ok').css("display", "block");
+    });
+
+    $(".inputText").hover(function() {
+    	$(this).siblings(".glyphicon-pencil").css("display","block");
+    	console.log("hello");
+    }, function() {
+    	$(this).siblings(".glyphicon-pencil").css("display","none");
+    });
+
+    $(".field-list").click(function() {
+    	$(this).children(".inputText").css("display", "none");
+    	$(this).children(".inputField").css("display", "block");
+    })
+
+});
