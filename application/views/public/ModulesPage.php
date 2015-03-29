ff156db9240f99c056894544f7a208061738533b
Nicholas Lum <nicholaslum444@hotmail.com>
2015-03-29 09:46:22 +0800

    A

--- application/views/public/ModulesPage.php
+++ application/views/public/ModulesPage.php
@@ -3,9 +3,14 @@
         <div class="col-sm-3 col-md-2 sidebar"><!--List of modules to be generated-->
             <ul class="nav nav-sidebar">
                 <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+                <!-- generate the list of modules for the sidebar -->
                 <?php for ($i = 0; $i < count($modules); $i++) {
                     $module = $modules[$i]; ?>
-                    <li><a href="/index.php/Modules/view/<?php echo $module['moduleCode']; ?>"><?php echo $module['moduleName']; ?></a></li>
+                    <li>
+                        <a href="/index.php/Modules/view/<?php echo $module['moduleCode']; ?>">
+                            <?php echo $module['moduleCode'] . " - " . $module['moduleName']; ?>
+                        </a>
+                    </li>
                 <?php } ?>
             </ul>
         </div>
@@ -13,14 +18,18 @@
         <div class="col-md-10">
             <div class="panel panel-default">
                 <div class="panel-body" id="moduleContent">
+                    <!-- show the module pane if a module is selected -->
                     <?php if (isset($selectedModule) && isset($projects)) { ?>
+                        <!-- module code, name and desc -->
                         <h1><?php echo $selectedModule["moduleCode"]; ?></h1>
-                        <h1><?php echo $selectedModule["moduleName"]; ?></h1>
+                        <h2><?php echo $selectedModule["moduleName"]; ?></h2>
+                        <p><?php echo $selectedModule["moduleDescription"]; ?></p>
+                        <!-- break -->
                         <p>List of Projects:</p>
                         <?php for ($i = 0; $i < count($projects); $i++) {
                             $project = $projects[$i];
                             $students = $projectDetails[$i]; ?>
-                            <!-- make a div for the project -->
+                            <!-- make a div for each project -->
                             <div class="col-sm-8">
                                 <h3><?php echo $project["title"]; ?></h3>
                                 <ul>
@@ -32,6 +41,10 @@
                                 <p><?php echo $project["abstract"]; ?></p>
                             </div>
                         <?php } ?>
+                    <?php } else { ?>
+                        <!-- else show the default page content for this page -->
+                        <h1>Participating Modules</h1>
+                        <p>Choose a module on the sidebar to see more information about it.</p>
                     <?php } ?>
                 </div>
             </div>
