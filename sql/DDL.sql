d2c76a3f4ce74cabd8bef15c430ab755ef5e2387
MunAw <jasontanma@gmail.com>
2015-03-30 14:36:13 +0800

    EDIT: updated database dummy data

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -5,15 +5,17 @@ DROP TABLE IF EXISTS project;
 DROP TABLE IF EXISTS module;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS STEPSiteration;
+DROP TABLE IF EXISTS admin;
+
 
 CREATE TABLE IF NOT EXISTS user (
-	matric_no VARCHAR(20),
+	user_id VARCHAR(20),
 	name VARCHAR(50) NOT NULL,
 	email VARCHAR(50),
 	contact INTEGER,
 	user_type INTEGER, 
 	food_preference INTEGER,
-	PRIMARY KEY(matric_no, user_type)
+	PRIMARY KEY(user_id, user_type)
 );
 
 CREATE TABLE IF NOT EXISTS module (
@@ -22,7 +24,9 @@ CREATE TABLE IF NOT EXISTS module (
 	class_size INTEGER,
 	module_name VARCHAR(100) NOT NULL,
 	module_description VARCHAR(2000),
-	PRIMARY KEY(module_code, iteration)
+	PRIMARY KEY(module_code, iteration),
+	FOREIGN KEY (iteration)
+		REFERENCES STEPSiteration(iteration)
 );
 
 CREATE TABLE IF NOT EXISTS project (
@@ -33,22 +37,24 @@ CREATE TABLE IF NOT EXISTS project (
 	project_id INTEGER AUTO_INCREMENT,
 	module_code VARCHAR(8) NOT NULL,
 	iteration INTEGER NOT NULL,
-	leader_matric_no CHAR(9),
+	leader_user_id CHAR(9),
 	PRIMARY KEY(project_id),
 	FOREIGN KEY(module_code, iteration) 
 	    REFERENCES module(module_code, iteration)
 	    ON UPDATE CASCADE,
-	FOREIGN KEY(leader_matric_no) 
-	    REFERENCES user(matric_no)
-	    ON UPDATE CASCADE
+	FOREIGN KEY(leader_user_id) 
+	    REFERENCES user(user_id)
+	    ON UPDATE CASCADE,
+	FOREIGN KEY (iteration)
+		REFERENCES STEPSiteration(iteration)
 );
 
 CREATE TABLE IF NOT EXISTS participate (
-	matric_no VARCHAR(20),
+	user_id VARCHAR(20),
 	project_id INTEGER,
-	PRIMARY KEY(matric_no, project_id),
-	FOREIGN KEY(matric_no) 
-		REFERENCES user(matric_no)
+	PRIMARY KEY(user_id, project_id),
+	FOREIGN KEY(user_id) 
+		REFERENCES user(user_id)
 		ON UPDATE CASCADE,
 	FOREIGN KEY(project_id) 
 		REFERENCES project(project_id) 
@@ -56,16 +62,18 @@ CREATE TABLE IF NOT EXISTS participate (
 );
 
 CREATE TABLE IF NOT EXISTS supervise (
-	matric_no VARCHAR(20), 
+	user_id VARCHAR(20), 
 	module_code VARCHAR(8), 
 	iteration INTEGER,
-	PRIMARY KEY(matric_no, module_code, iteration),
-	FOREIGN KEY(matric_no) 
-		REFERENCES user(matric_no)
+	PRIMARY KEY(user_id, module_code, iteration),
+	FOREIGN KEY(user_id) 
+		REFERENCES user(user_id)
 		ON UPDATE CASCADE,
 	FOREIGN KEY(module_code, iteration) 
 		REFERENCES module(module_code, iteration)
-		ON UPDATE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (iteration)
+		REFERENCES STEPSiteration(iteration)
 );
 
 CREATE TABLE IF NOT EXISTS STEPSiteration (
@@ -75,14 +83,25 @@ CREATE TABLE IF NOT EXISTS STEPSiteration (
 );
 
 CREATE TABLE IF NOT EXISTS enrolled (
-	matric_no VARCHAR(20),
+	user_id VARCHAR(20),
 	module_code VARCHAR(8), 
 	iteration INTEGER,
-	PRIMARY KEY(matric_no, module_code, iteration),
-	FOREIGN KEY(matric_no) 
-		REFERENCES user(matric_no)
+	PRIMARY KEY(user_id, module_code, iteration),
+	FOREIGN KEY(user_id) 
+		REFERENCES user(user_id)
 		ON UPDATE CASCADE,
 	FOREIGN KEY(module_code, iteration) 
 		REFERENCES module(module_code, iteration)
-		ON UPDATE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (iteration)
+		REFERENCES STEPSiteration(iteration)
+);
+
+CREATE TABLE IF NOT EXISTS admin (
+	user_id VARCHAR(30),
+	password VARCHAR(30),
+	name VARCHAR(50),
+	email VARCHAR(50),
+	contact INTEGER,
+	PRIMARY KEY(user_id,password)
 );
--- sql/prof.sql
+++ sql/prof.sql
@@ -1,1 +1,1 @@
-INSERT INTO `user` (`matric_no`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES ("A0101095B","Steven Halim","stevenha@nus.edu.sg",98798765,2,3),("A0101085B","Ben Leong","benleong@gmail.com",98777777,2,4),("A0101075B","Anand Bhojan","anandb@nus.edu.sg",93398765,2,3);
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES ("A0101095B","Steven Halim","stevenha@nus.edu.sg",98798765,2,3),("A0101085B","Ben Leong","benleong@gmail.com",98777777,2,4),("A0101075B","Anand Bhojan","anandb@nus.edu.sg",93398765,2,3);
