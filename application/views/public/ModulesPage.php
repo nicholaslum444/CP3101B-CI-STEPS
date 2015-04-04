147d6b3733706cf0bb9ac33fa325ba50d218cc00
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-05 00:57:07 +0800

    Improve the module page
    1. Pushed the sidebar out by 1 column
    2. Shrunk the module pane to 7 columns
    3. Improved the module pane header
    4. Improved the projects panes
    
    Further issues:
    1. Sidebar goes under the module pane when screen too small

--- application/views/public/ModulesPage.php
+++ application/views/public/ModulesPage.php
@@ -1,16 +1,21 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
+
+        <div class="col-xs-1">
+            <!-- nothing inside here -->
+        </div>
+
+        <div class="col-xs-2 sidebar">
             <ul class="nav nav-sidebar wrap-sidebar">
                 <li class="active"><a href="/index.php/Modules/view">Overview <span class="sr-only">(current)</span></a></li>
                 <!-- generate the list of modules for the sidebar -->
                 <?php
-                foreach($allModulesDetails as $details) { ?>
+                foreach ($allModulesDetails as $details) { ?>
                     <li>
                         <a href="/index.php/Modules/view/<?php echo $details['moduleCode']; ?>">
                             <span class="sidebar-module-code"><?php echo $details['moduleCode']; ?></span>
-                            <span class="sidebar-separator"> - </span>
-                            <span class="sidebar-module-name"><?php echo $details['moduleName']; ?></span>
+                            <span class="sidebar-separator"><br></span>
+                            <small class="sidebar-module-name"><?php echo $details['moduleName']; ?></small>
                         </a>
                     </li>
                     <?php
@@ -18,42 +23,76 @@
             </ul>
         </div>
 
-        <div class="col-md-10">
-            <div class="panel panel-default">
+        <div class="col-xs-7 panel">
+            <div clss="panel panel-default">
                 <div class="panel-body" id="moduleContent">
                     <!-- show the module pane if a module is selected -->
-                    <?php if (!is_null($selectedModuleData)) { ?>
-                        <!-- module code, name and desc -->
-                        <h1><?php echo $selectedModuleData["moduleCode"]; ?></h1>
-                        <h2><?php echo $selectedModuleData["moduleName"]; ?></h2>
-                        <p><?php echo $selectedModuleData["moduleDescription"]; ?></p>
-                        <!-- break -->
-                        <p>List of Projects:</p>
-                        <?php
+                    <?php if (!is_null($selectedModuleData)) {
                         $projects = $selectedModuleData["projectList"];
-                        foreach($projects as $project) { ?>
-                            <!-- make a div for each project -->
-                            <div class="col-sm-8">
-                                <h3><?php echo $project["title"]; ?></h3>
-                                <ul>
-                                    <?php
-                                    if (isset($project["members"])) {
-                                        $students = $project["members"];
-                                    } else {
-                                        $students = [];
-                                    }
-                                    foreach($students as $student) {
-                                        ?>
-                                        <li>
-                                            <?php echo $student["name"]; ?> - <?php echo $student["userID"]; ?>
-                                        </li>
-                                    <?php } ?>
-                                </ul>
-                                <p>
-                                    <?php echo $project["abstract"]; ?>
-                                </p>
-                            </div>
-                        <?php } ?>
+                        $numProjects = count($projects);
+                        //echo json_encode($projects);
+                        $numStudents = 0;
+                        foreach ($projects as $project) {
+                            // sum the total students
+                            $numStudents += count($project["members"]);
+                        }
+                        // TODO replace with real variable!!
+                        $moduleLecturer = "Dr. STEVEN HALIM";
+                        ?>
+                        <!-- module code, name and desc -->
+                        <div class="page-header">
+                            <h1>
+                                <?php echo $selectedModuleData["moduleCode"]; ?>
+                                <small><br>
+                                    <?php echo $selectedModuleData["moduleName"]; ?>
+                                </small>
+                            </h1>
+                            <p><strong>
+                                Chaired by
+                                <?php echo $moduleLecturer ?>
+                            </strong></p>
+                            <p><em>
+                                <?php echo $numStudents ?>
+                                students in
+                                <?php echo $numProjects ?>
+                                groups
+                            </em></p>
+                            <?php echo $selectedModuleData["moduleDescription"]; ?>
+                        </div>
+                        <!-- break -->
+                        <h3>Projects</h3>
+                        <div class="projects-container">
+                            <?php
+                            foreach ($projects as $project) { ?>
+                                <!-- make a div for each project -->
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <h3 class="panel-title"><?php echo $project["title"]; ?></h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <p>
+                                            <?php echo $project["abstract"]; ?>
+                                        </p>
+                                    </div>
+                                    <ul class="list-group">
+                                        <?php
+                                        if (isset($project["members"])) {
+                                            $students = $project["members"];
+                                        } else {
+                                            $students = [];
+                                        }
+                                        foreach($students as $student) {
+                                            ?>
+                                            <li class="list-group-item">
+                                                <?php echo $student["name"]; ?>
+                                                <!-- -  -->
+                                                <!-- <?php echo $student["userID"]; ?> -->
+                                            </li>
+                                        <?php } ?>
+                                    </ul>
+                                </div>
+                            <?php } ?>
+                        </div>
                     <?php } else { ?>
                         <!-- else show the default page content for this page -->
                         <h1>Participating Modules</h1>
