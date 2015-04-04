d58c6320a6e898d06e708aff93ce7415d38e791c
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-05 01:44:39 +0800

    Improved the module page for some fields
    1. Missing data (projects, members) is shown as info msg
    2. Improved the default modules desc and titles for realism

--- application/views/public/ModulesPage.php
+++ application/views/public/ModulesPage.php
@@ -27,7 +27,8 @@
             <div clss="panel panel-default">
                 <div class="panel-body" id="moduleContent">
                     <!-- show the module pane if a module is selected -->
-                    <?php if (!is_null($selectedModuleData)) {
+                    <?php
+                    if (!is_null($selectedModuleData)) {
                         $projects = $selectedModuleData["projectList"];
                         $numProjects = count($projects);
                         //echo json_encode($projects);
@@ -39,6 +40,7 @@
                         // TODO replace with real variable!!
                         $moduleLecturer = "Dr. STEVEN HALIM";
                         ?>
+
                         <!-- module code, name and desc -->
                         <div class="page-header">
                             <h1>
@@ -59,47 +61,79 @@
                             </em></p>
                             <?php echo $selectedModuleData["moduleDescription"]; ?>
                         </div>
-                        <!-- break -->
+
+                        <!-- projects section -->
                         <h3>Projects</h3>
-                        <div class="projects-container">
+                        <div class="projects-container panel">
                             <?php
-                            foreach ($projects as $project) { ?>
-                                <!-- make a div for each project -->
-                                <div class="panel panel-primary">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title"><?php echo $project["title"]; ?></h3>
-                                    </div>
-                                    <div class="panel-body">
-                                        <p>
-                                            <?php echo $project["abstract"]; ?>
-                                        </p>
-                                    </div>
-                                    <ul class="list-group">
-                                        <?php
-                                        if (isset($project["members"])) {
-                                            $students = $project["members"];
-                                        } else {
-                                            $students = [];
-                                        }
-                                        foreach($students as $student) {
-                                            ?>
-                                            <li class="list-group-item">
-                                                <?php echo $student["name"]; ?>
-                                                <!-- -  -->
-                                                <!-- <?php echo $student["userID"]; ?> -->
-                                            </li>
-                                        <?php } ?>
-                                    </ul>
+                            if (count($projects) <= 0) {
+                                // if no projects
+                                ?>
+                                <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <p>
+                                        Projects info not yet available!
+                                    </p>
                                 </div>
-                            <?php } ?>
+                                <?php
+                            } else {
+                                // there are projects
+                                foreach ($projects as $project) {
+                                    ?>
+                                    <!-- make a div for each project -->
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title"><?php echo $project["title"]; ?></h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <p>
+                                                <?php echo $project["abstract"]; ?>
+                                            </p>
+                                        </div>
+                                        <ul class="list-group">
+                                            <li class="list-group-item">
+                                                <h3 class="list-group-item-heading"><small><strong>
+                                                    Members
+                                                </strong></small></h4>
+                                            </li>
+                                            <?php
+                                            if (isset($project["members"])) {
+                                                $students = $project["members"];
+                                                foreach($students as $student) {
+                                                    ?>
+                                                    <li class="list-group-item">
+                                                        <?php echo $student["name"]; ?>
+                                                        <!-- -  -->
+                                                        <!-- <?php //echo $student["userID"]; ?> -->
+                                                    </li>
+                                                    <?php
+                                                }
+                                            } else {
+                                                //show that there are no students!
+                                                ?>
+                                                <li class="list-group-item disabled">Group members info not available!</li>
+                                                <?php
+                                            }
+                                            ?>
+                                        </ul>
+                                    </div>
+                                    <?php
+                                }
+                            }
+                            ?>
                         </div>
-                    <?php } else { ?>
+                        <?php
+                    } else {
+                        ?>
                         <!-- else show the default page content for this page -->
                         <h1>Participating Modules</h1>
                         <p>Choose a module on the sidebar to see more information about it.</p>
-                    <?php } ?>
+                        <?php
+                    }
+                    ?>
                 </div>
             </div>
         </div>
+
     </div>
 </div> <!-- End of container -->
