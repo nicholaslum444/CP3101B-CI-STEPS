9608fb036342c72e83f2289971c80ef845ed1844
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-07 02:03:58 +0800

    Ported over the sponsors page

---
+++ application/views/public/SponsorsPage.php
@@ -0,0 +1,121 @@
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron afternav">
+    <div class="container">
+        <h1>STePS<br>
+            <small>
+                <strong>S</strong>chool of Computing
+                <strong>Te</strong>rm <strong>P</strong>roject <strong>S</strong>howcase
+            </small>
+        </h1>
+    </div>
+</div>
+
+<div class="container">
+    <div class="container-fluid">
+        <section id="top">
+            <h1>Sponsors</h1>
+
+            <p>
+                STePS is only possible with the generous support of corporate
+                and government sponsorship. We'd like to thank them for their
+                support here!
+            </p>
+
+            <p>
+                Please click on the logos for our sponsors to visit their
+                website and learn more about their organization and products.
+            </p>
+
+        </section>
+        <section id="p">
+            <h2>Platinum Sponsors</h2>
+
+            <a href="http://ida.gov.sg">
+                <img src="//steps.comp.nus.edu.sg/steps6/img/ida.h.jpg" longdesc="http://ida.gov.sg/" alt="IDA" width="300" style="padding:10px" title="IDA">
+            </a>
+            <p>
+                &nbsp;&nbsp;
+                <font size="-1"><i>
+                    Info-Comm Development Authority of Singapore (<a href="http://www.ida.gov.sg/">http://ida.gov.sg/</a>)
+                </i></font>
+            </p>
+
+            <a href="http://www.garena.com">
+                <img src="//steps.comp.nus.edu.sg/steps6/img/GARENA_LOGO.png" alt"garena"="" width="300" style="padding:10px" title="Garena">
+            </a>
+            <p>
+                &nbsp;&nbsp;
+                <font size="-1"><i>
+                    Garana Online (<a href="http://www.garena.com">http://www.garena.com/</a>)
+                    Garena is one of the largest Internet companies in Southeast Asia and Taiwan. Headquartered in Singapore, it employs over 2500 people across the region. Its services includes Payment, E-commerce, Gaming and Social network. On any single day, more than 10 million customers use its services. It prides itself as a software engineering centric company and has one of the strongest R&amp;D team in the region.
+                </i></font>
+            </p>
+
+            <a href="http://www.smaato.com/">
+                <img src="//steps.comp.nus.edu.sg/steps6/img/smaato_logo.png" longdesc="http://www.smaato.com/" alt="Smaato" width="350" style="padding:10px" title="Smaato">
+            </a>
+            <p> &nbsp;&nbsp;
+                <font size="-1"><i>
+                    Smaato (<a href="http://www.smaato.com/">http://www.smaato.com/</a>) is the leading global mobile RTB ad exchange (SMX) helping mobile app developers and publishers increase ad revenues worldwide. As an industry pioneer and leader, Smaato operates the leading mobile RTB ad exchange and Supply Side Platform across 80,000+ mobile app developers and mobile publishers. On the demand side, Smaato globally connects 138+ ad networks and 199+ DSPs. <br>
+
+                    &nbsp;&nbsp;
+                    Smaato is an initial member of the OpenRTB Mobile subcommittee and an active member of the Mobile Marketing Association, Mobile Entertainment Forum (MEF), Singapore Infocomm Industry (SITF), Singapore IT Federation and the German Digital Media Association BVDW. <br>
+
+                    &nbsp;&nbsp;
+                    Smaato received a Top 100 Private Company Award by AlwaysOn Media (2014, 2013, 2012, 2011, 2009 &amp; 2007), is one of the AlwaysOn Global 250 winners in the Mobile category (2013, 2012 &amp; 2011) and was named a “company to watch in 2010″ by Financial Analyst Company GP Bullhound. <br>
+
+                    &nbsp;&nbsp;
+                    Smaato´s global headquarters is located in San Francisco, California. The privately held company was founded in 2005 by an experienced International management team. The Smaato European office is located in Hamburg, Germany and the APAC office in Singapore. <br>
+                </i></font>
+            </p>
+            <p class="pull-right"><a href="#top">Back to top</a></p>
+        </section>
+
+        <section id="s">
+            <h2>Silver Sponsors</h2>
+
+            <div style="text-align:center">
+                <a href="http://www.easybook.com"><img src="//steps.comp.nus.edu.sg/steps6/img/easybook.png" longdesc="http://www.easybook.com" width="330"></a>
+                <a href="http://www.uxarmy.com/"><img src="//steps.comp.nus.edu.sg/steps6/img/uxarmy.png" alt="UXArmy" longdesc="http://www.uxarmy.com/"> </a>
+            </div>
+        </section>
+
+        <section id="ik">
+            <h2>In-Kind Discovery Sponsorship</h2>
+
+            <div style="text-align:center">
+
+                <a href="http://www.gopaktor.com/"><img src="//steps.comp.nus.edu.sg/steps6/img/paktormedium.png" alt="Paktor" title="Paktor" style="padding:10px" width="120px"></a>
+                <a href="http://www.bluelambda.com.sg"><img src="//steps.comp.nus.edu.sg/steps6/img/bluelambda.s.png" alt="BlueLambda" title="BlueLambda" style="padding:10px"></a>
+                <a href="http://www.drive.sg/"><img src="//steps.comp.nus.edu.sg/steps6/img/drivesg_white_s.png" alt="Drive.SG" title="Drive.SG" style="padding:10px"></a>
+                <a href="https://www.facebook.com/deltaduckstudios/"><img src="//steps.comp.nus.edu.sg/steps6/img/deltaduckmedium.png" alt="Delta Duck Studios" title="Delta Duck Studios" style="padding:10px" width="130px"></a>
+                <a href="http://www.klinify.com/"><img src="//steps.comp.nus.edu.sg/steps6/img/klinify_logo_s.jpg" alt="Klinify" title="Klinify" style="padding:10px"></a>
+                <a href="http://metalworks.maxusglobal.com/"> <img src="//steps.comp.nus.edu.sg/steps6/img/metalworks-logo.png" longdesc="http://metalworks.maxusglobal.com/" alt="Metalworks by Maxus" title="Metalworks by Maxus" style="padding:10px" width="200"> </a>
+                <a href="http://www.websparks.sg/"><img src="//steps.comp.nus.edu.sg/steps6/img/logo_websparks.png" width="200" alt="Web Sparks" longdesc="http://www.websparks.sg/"> </a>
+                <a href="http://www.kingdom-crafting.co/"><img src="//steps.comp.nus.edu.sg/steps6/img/kc-mediumlogo.png" alt="Kingdom-Crafting" title="Kingdom-Crafting" style="padding:10px" width="160px"></a>
+                <a href="https://www.visenze.com/"><img src="//steps.comp.nus.edu.sg/steps6/img/ViSenzeMedium.png" alt="Visenze" title="Visenze" style="padding:10px" width="160px"></a>
+
+                <a href="http://icreate.nus.edu.sg/"><img src="//steps.comp.nus.edu.sg/steps6/img/logoicreate.png" alt="iCreate" width="150" style="padding:10px" title="NUS iCreate"> </a>
+
+                <a href="http://enterprise.nus.edu.sg/"><img src="//steps.comp.nus.edu.sg/steps6/img/NUS_Enterprise-Logo.png" alt="NUS Enterprise" width="200" style="padding:10px" title="NUS Enterprise"> </a>
+
+                <a href="http://nuscaa.sg"><img src="//steps.comp.nus.edu.sg/steps6/img/caa.s.png" alt="Computing Alumni Association" title="Computing Alumni Association" style="padding:10px"> </a>
+
+            </div>
+            <p class="pull-right"><a href="#top">Back to top</a></p>
+        </section>
+
+        <section id="deck">
+            <h2>Sponsorship Deck</h2>
+
+            <div style="text-align:left">
+
+                If you are interested in partially sponsoring current or next STePS event, please contact the STePS Coordinator: <strong> Dr. Bhojan Anand </strong>, banand [at] comp.nus.edu.sg.
+
+
+            </div>
+            <p class="pull-right"><a href="#top">Back to top</a></p>
+        </section>
+
+    </div>
+</div>
