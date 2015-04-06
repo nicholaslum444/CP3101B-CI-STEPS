acebcf12d68f9b1444a6597efc8e161c21ac2e39
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-06 23:08:02 +0800

    Modified the home page data to be more realistic
    need to work on module page

--- application/views/public/HomePage.php
+++ application/views/public/HomePage.php
@@ -23,8 +23,8 @@
             <!-- to put image beside words -->
             <div class="media">
                 <div class="media-left media-middle">
-                    <a href="">
-                        <img height="240px" width="180px" class="media-object" src="/img/6thStePS.jpg" alt="steps poster">
+                    <a>
+                        <img height="400px" width="300px" class="media-object" src="/img/6thStePS.jpg" alt="steps poster">
                     </a>
                 </div>
                 <div class="media-body">
@@ -48,19 +48,27 @@
             <!-- to put image beside words -->
             <div class="media">
                 <div class="media-left media-middle">
-                    <a href="">
-                        <img height="60x" width="90px" class="media-object" src="/img/participants.jpg" alt="steps poster">
+                    <a>
+                        <img height="200x" width="300px" class="media-object" src="/img/participants.jpg" alt="steps poster">
                     </a>
                 </div>
                 <div class="media-body">
                     <p>
-                        Students, faculty, technology entrepreneurs and prospective students will have the opportunity to experience the innovations created by the students. EIGHT courses are participating in the Term Project Showcase this semester.
+                        Students, faculty, technology entrepreneurs and prospective students will have the opportunity to experience the innovations created by the students.
+                        95 innovative projects from 10 participating courses/tracks will be showcased in the Term Project Showcase this semester.
                     </p>
                     <!-- best if we can dynamic this as well -->
                     <ul>
+                        <li>IS 5126 Hands-On With Business Analytics (Chair: Prof. Tuan Q. Phan)</li>
+                        <li>CS 3217 Software Engineering on Modern Application Platforms (Chair: Prof. Sim Khe Chai)</li>
+                        <li>CS 3218 Multimodal Processing in Mobile Platforms (Chair: Prof. Ng Teck Khim)</li>
+                        <li>CS 3240 Interaction Design (Chair: Dr. Bimlesh Wadhwa)</li>
+                        <li>CS 3247 Game Development (Chair: Dr. Bhojan Anand)</li>
+                        <li>CS 3284 Media Technology Project II (Chair: Prof. Leow Wee-Kheng and Prof. Terrence Sim)</li>
                         <li>CS 4244 Knowledge-Based Systems (Chair: Prof. Tan Chew Lim)</li>
                         <li>CS 4344 Networked and Mobile Gaming (Chair: Prof. Ooi wei Tsang)</li>
                         <li>CP 3101B Web programming and Applications (Chair: Dr. Steven Halim)</li>
+                        <li>Selected FYPs Final Year Project (Chair: Prof. Terrence Sim)</li>
                     </ul>
                     <p>
                         The event is organised by NUS School of Compuiting with strong support from our Industry and Academia partners. It is estimated that about 800+ guests (students, professors, industry partners) will attaned the 6th STePS.
@@ -77,13 +85,17 @@
                 Registration
             </h3>
         </div>
-        <div class="panel-body" style="text-align:center">
-            <iframe src="//eventbrite.sg/tickets-external?eid=16153884704&amp;ref=etckt" frameborder="0" height="250" width="80%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
-            <p>
-                Takes about 2 minute of your time! <br>
-                Food and drink will be provided to registered participants. <br>
-                Last date for registration of Standard Tickets with buffet dinner: Apr 13, 2015 6:00:00 PM
-            </p>
+        <div class="panel-body">
+            <div class="registration-text-container">
+                <p>
+                    Register and get your ticket for the show. It takes about 2 minute of your time!  <br>
+                    Food and drink will be provided to registered participants. <br>
+                    <span style="color:#3498db">Last date for registration of Standard Tickets with buffet dinner: Apr 13, 2015 6:00:00 PM</span>
+                </p>
+            </div>
+            <div class="registration-iframe-container">
+                <iframe src="//eventbrite.sg/tickets-external?eid=16153884704&amp;ref=etckt" frameborder="0" height="250" width="80%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+            </div>
         </div>
     </div> <!-- end panel -->
 
