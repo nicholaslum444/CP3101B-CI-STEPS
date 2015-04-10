ce2567362cd00533a9ffb370276dc92c17bcf587
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-10 23:11:51 +0800

    Major improvement to home page but still hardcoded

--- application/views/persistent/SiteHeader.php
+++ application/views/persistent/SiteHeader.php
@@ -39,13 +39,17 @@ switch($loader) {
     <!-- <link rel="stylesheet" href="/css/bootstrap.min.css"> -->
     <!-- <link rel="stylesheet" href="/css/bootstrap-theme.min.css"> -->
     <link rel="stylesheet" href="/css/bootswatch-flatly.css">
+    <!-- <link rel="stylesheet" href="/css/jqueryuk15.css"> -->
+    <link rel="stylesheet" href="/css/jqueryuk15extracted.css">
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/font-awesome/css/font-awesome.min.css">
     <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700' rel='stylesheet' type='text/css'>
 
     <script src="/js/jquery-2.1.3.min.js"></script>
     <script src="/js/bootstrap.js"></script>
     <script src="/js/main.js"></script>
+    <script src="/js/ui.js"></script>
 
     <script>
     // for the binding later
@@ -58,6 +62,17 @@ switch($loader) {
 </head>
 
 <body>
+
+    <!-- facebook like code -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=287314788099485";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <!-- end facebook like -->
 
     <?php
     // creates the modal login dialogs for un-logged-in non-admins
@@ -97,32 +112,26 @@ switch($loader) {
     ?>
 
     <!-- include the navbar as well -->
-    <nav class="navbar navbar-fixed-top navbar-default" role="navigation">
-        <div class="container">
-
-            <!-- contains the collapse and the brand name -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <ul class="nav navbar-nav navbar-left dropdown">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle navbar-brand"><?php echo $iteration ?>th STePS&nbsp;<span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="/">The new STePS</a></li>
-                        <li><a href="//steps.comp.nus.edu.sg">STePS Classic</a></li>
-                    </ul>
-                </ul>
+    <header class="navbar navbar-fixed-top navbar-default site__header" role="navigation">
+        <!-- <div class="nav__contain"> -->
+            <div class="container">
+                <a href="/index.php" class="navbar-brand"><?php echo $iteration ?>th STePS&nbsp;</a>
+                <!-- contains the collapse and the brand name -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
                 <!-- <a class="navbar-brand" href="/index.php">
                     <?php echo $iteration ?>th STePS
                 </a> -->
-            </div>
-
-            <div id="navbar" class="navbar-collapse collapse">
-                <!-- links to public pages -->
-                <!-- <div class="navbar-left">  -->
+                <!--navbar-collapse collapse centered-navbar-->
+                <div id="navbar" class="navbar-collapse collapse list-inline nav--primary u-uppercase">
+                    <!-- links to public pages -->
+                    <!-- <div class="navbar-left">  -->
                     <ul class="nav navbar-nav navbar-left">
                         <li class="<?php echo $modulesActive; ?>">
                             <a href="/index.php/modules">Modules</a>
@@ -134,46 +143,69 @@ switch($loader) {
                             <a href="/index.php/sponsors">Sponsors</a>
                         </li>
                     </ul>
-                <!-- </div> -->
+                    <!-- </div> -->
 
-                <!-- sign-in boxes -->
-                <div class="navbar-right navbar-login-container">
-                    <?php
-                    // (messy) code to load the correct buttons
-                    // based on whether user logged in or not
-                    $buttonType = "success";
-                    if ($isLoggedIn) {
-                        if ($userType === USER_TYPE_LECTURER) {
-                            $userType = "Lecturer";
-                            $buttonType = "info";
-                        } else if ($userType === USER_TYPE_STUDENT) {
-                            $userType = "Student";
-                        } else if ($userType === USER_TYPE_ADMIN) {
-                            $userType = "Admin";
+                    <!-- sign-in boxes -->
+                    <ul class="nav navbar-nav navbar-right dropdown">
+                        <?php
+                        // (messy) code to load the correct buttons
+                        // based on whether user logged in or not
+                        $buttonType = "success";
+                        if ($isLoggedIn) {
+                            if ($userType === USER_TYPE_LECTURER) {
+                                $userType = "Lecturer";
+                                $buttonType = "info";
+                            } else if ($userType === USER_TYPE_STUDENT) {
+                                $userType = "Student";
+                            } else if ($userType === USER_TYPE_ADMIN) {
+                                $userType = "Admin";
+                            }
+                            ?>
+                            <li>
+                                <a href="#" data-toggle="dropdown" class="dropdown-toggle" aria-expanded="true">
+                                    <?php echo $name; ?>
+                                    <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="/index.php/<?php echo $userType; ?>/console">
+                                            <button class="btn btn-<?php echo $buttonType; ?> navbar-btn">Console</button>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="/index.php/logout">
+                                            <button class="btn btn-danger navbar-btn">Logout</button>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <?php
+                        } else if (!(isset($loader) && $loader === LOADER_TYPE_ADMIN)) {
+                            ?>
+                            <li data-toggle="dropdown" class="dropdown-toggle" aria-expanded="true">
+                                <a href="#" data-toggle="dropdown" class="dropdown-toggle" aria-expanded="true">
+                                    Log in as:&nbsp;
+                                    <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <button class="btn btn-success navbar-btn" id="loginBtnStudent" data-toggle="modal" data-target="#ivleStudentModal" data-backdrop="static">
+                                            Student
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button class="btn btn-info navbar-btn" id="loginBtnLecturer" data-toggle="modal" data-target="#ivleLecturerModal" data-backdrop="static">
+                                            Lecturer
+                                        </button>
+                                    </li>
+                                </ul>
+                            </li>
+                            <!-- <span class="navbar-text navbar-login-text">Log in as:&nbsp;</span> -->
+                            <?php
                         }
                         ?>
-                        <a href="/index.php/<?php echo $userType; ?>/console">
-                            <button class="btn btn-<?php echo $buttonType; ?> navbar-btn"><?php echo $name; ?></button>
-                        </a>
-                        <a href="/index.php/logout">
-                            <button class="btn btn-danger navbar-btn">Logout</button>
-                        </a>
-                        <?php
-                    } else if (!(isset($loader) && $loader === LOADER_TYPE_ADMIN)) {
-                        ?>
-                        <p class="navbar-text navbar-login-text">Sign in as:</p>
-                        <button class="btn btn-success navbar-btn" id="loginBtnStudent"
-                        data-toggle="modal" data-target="#ivleStudentModal" data-backdrop="static">
-                            Student
-                        </button>
-                        <button class="btn btn-info navbar-btn" id="loginBtnLecturer"
-                        data-toggle="modal" data-target="#ivleLecturerModal" data-backdrop="static">
-                            Lecturer
-                        </button>
-                        <?php
-                    }
-                    ?>
-                </div>
-            </div><!--/.navbar-collapse -->
-        </div>
-    </nav>
+                    </ul>
+                </div><!--/.navbar-collapse -->
+            </div>
+        <!-- </div> -->
+    </header>
--- application/views/public/HomePage.php
+++ application/views/public/HomePage.php
@@ -1,122 +1,196 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron afternav">
-    <div class="container">
-        <h1>STePS<br>
-            <small>
-                <strong>S</strong>chool of Computing
-                <strong>Te</strong>rm <strong>P</strong>roject <strong>S</strong>howcase
-            </small>
-        </h1>
+<div class="hero afternav">
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+            <h1 class="hero__heading">STePS</h1>
+            <h2 class="hero__subheading">School of Computing's Term Project Showcase</h2>
+            <a class="btn btn-default btn-lg" href="/index.php/About">
+                Learn More
+            </a>
+        </div>
     </div>
 </div>
 
 <div class="container">
 
-    <!-- about -->
-    <div class="panel panel-primary">
-        <div class="panel-heading">
-            <h3 class="panel-title">
-                About STePS
-            </h3>
-        </div>
-        <div class="panel-body">
-            <!-- to put image beside words -->
-            <div class="media">
-                <div class="media-left media-middle">
-                    <a>
-                        <img height="400px" width="300px" class="media-object" src="/img/6thStePS.jpg" alt="steps poster">
-                    </a>
-                </div>
-                <div class="media-body">
+    <!-- location, date and time information -->
+    <div class="block--half-spaced">
+        <ul class="list-stylised list-jumbo u-uppercase">
+            <li>
+                <span class="glyphicon glyphicon-globe"></span>
+                <!-- need php -->
+                SoC COM1
+            </li>
+            <li>
+                <span class="glyphicon glyphicon-calendar"></span>
+                <!-- need php -->
+                22 April 2015
+            </li>
+            <li>
+                <span class="glyphicon glyphicon-time"></span>
+                <!-- need php -->
+                6pm to 10pm
+            </li>
+        </ul>
+    </div>
+
+    <!-- event short info -->
+    <div class="block--spaced">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h2 class="section__heading">About The Showcase</h2>
+                <div class="u-center">
                     <p>
-                        The School of Computing Term Project Showcase (STePS) aims to bring together all of the class-based projects and project module in the School of Computing (SoC). It's a festive yet serious class showcase, in which students present their projects in all aspects of computer science and information systems to the respective Faculty for grading and to Industry guests, Government agencies, Sponsors and Investors to connect with potential employers and to seek opportunities for further development of their work through collaboration.
+                        The School of Computing Term Project Showcase (STePS) aims to bring together all of the class-based projects and project module in the School of Computing (SoC).
+                    </p>
+                    <p>
+                        Get the <a href="">Programme Booklet</a> or view our <a href="">Poster Invitation</a>
                     </p>
                 </div>
-            </div> <!-- end media -->
-        </div>
-    </div> <!-- end panel -->
-
-
-    <!-- participants -->
-    <div class="panel panel-primary">
-        <div class="panel-heading">
-            <h3 class="panel-title">
-                Participants
-            </h3>
-        </div>
-        <div class="panel-body">
-            <!-- to put image beside words -->
-            <div class="media">
-                <div class="media-left media-middle">
-                    <a>
-                        <img height="200x" width="300px" class="media-object" src="/img/participants.jpg" alt="steps poster">
-                    </a>
-                </div>
-                <div class="media-body">
-                    <p>
-                        Students, faculty, technology entrepreneurs and prospective students will have the opportunity to experience the innovations created by the students.
-                        95 innovative projects from 10 participating courses/tracks will be showcased in the Term Project Showcase this semester.
-                    </p>
-                    <!-- best if we can dynamic this as well -->
-                    <ul>
-                        <li>IS 5126 Hands-On With Business Analytics (Chair: Prof. Tuan Q. Phan)</li>
-                        <li>CS 3217 Software Engineering on Modern Application Platforms (Chair: Prof. Sim Khe Chai)</li>
-                        <li>CS 3218 Multimodal Processing in Mobile Platforms (Chair: Prof. Ng Teck Khim)</li>
-                        <li>CS 3240 Interaction Design (Chair: Dr. Bimlesh Wadhwa)</li>
-                        <li>CS 3247 Game Development (Chair: Dr. Bhojan Anand)</li>
-                        <li>CS 3284 Media Technology Project II (Chair: Prof. Leow Wee-Kheng and Prof. Terrence Sim)</li>
-                        <li>CS 4244 Knowledge-Based Systems (Chair: Prof. Tan Chew Lim)</li>
-                        <li>CS 4344 Networked and Mobile Gaming (Chair: Prof. Ooi wei Tsang)</li>
-                        <li>CP 3101B Web programming and Applications (Chair: Dr. Steven Halim)</li>
-                        <li>Selected FYPs Final Year Project (Chair: Prof. Terrence Sim)</li>
-                    </ul>
-                    <p>
-                        The event is organised by NUS School of Compuiting with strong support from our Industry and Academia partners. It is estimated that about 800+ guests (students, professors, industry partners) will attaned the 6th STePS.
-                    </p>
-                </div>
-            </div> <!-- end media -->
-        </div>
-    </div> <!-- end panel -->
-
-    <!-- register -->
-    <div class="panel panel-primary">
-        <div class="panel-heading">
-            <h3 class="panel-title">
-                Registration
-            </h3>
-        </div>
-        <div class="panel-body">
-            <div class="registration-text-container">
-                <p>
-                    Register and get your ticket for the show. It takes about 2 minute of your time!  <br>
-                    Food and drink will be provided to registered participants. <br>
-                    <span style="color:#3498db">Last date for registration of Standard Tickets with buffet dinner: Apr 13, 2015 6:00:00 PM</span>
-                </p>
             </div>
-            <div class="registration-iframe-container">
-                <iframe src="//eventbrite.sg/tickets-external?eid=16153884704&amp;ref=etckt" frameborder="0" height="250" width="80%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
-            </div>
-        </div>
-    </div> <!-- end panel -->
-
-
-</div>
-
-<div class="container" style="display:none">
-    <div class="row">
-        <div class="col-md-4">
-            <h2>For easy testing & navigation</h2>
-            <p>Admin </p>
-            <p><a class="btn btn-success" href="/index.php/admin" role="button">View details</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Lecturer</h2>
-            <p><a class="btn btn-success" href="/index.php/lecturer/console" role="button">View details</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Student</h2>
-            <p><a class="btn btn-success" href="/index.php/student/console" role="button">View details</a></p>
         </div>
     </div>
 
-</div> <!-- End of container -->
+    <hr>
+
+    <!-- module thumbnails -->
+    <div class="block--spaced u-center">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h2 class="section__heading">Participating Modules</h2>
+                <div class="u-center">
+                    <p>
+                        Students, faculty, technology entrepreneurs and prospective students will have the opportunity to experience the innovations created by the students.
+                        <br>
+                        95 <!-- use php! -->
+                        innovative projects from
+                        10 <!-- use php! -->
+                        participating courses/tracks will be showcased in the Term Project Showcase this semester.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3">
+                <figure class="module-thumb">
+                    <a href="">
+                        <div class="module-thumb-img">
+                            <img src="/img/test.jpg">
+                        </div>
+                        <figcaption class="module-thumb-caption">
+                            <span class="module-thumb-caption-header">CS4203</span>
+                            <br>
+                            <span class="module-thumb-caption-text">SOUND AND MUSIC COMPUTING</span>
+                        </figcaption>
+                    </a>
+                </figure>
+            </div>
+            <div class="col-sm-3">
+                <figure class="module-thumb">
+                    <a href="">
+                        <div class="module-thumb-img">
+                            <img src="/img/test.jpg">
+                        </div>
+                        <figcaption class="module-thumb-caption">
+                            <span class="module-thumb-caption-header">CS4203</span>
+                            <br>
+                            <span class="module-thumb-caption-text">SOUND AND MUSIC COMPUTING</span>
+                        </figcaption>
+                    </a>
+                </figure>
+            </div>
+            <div class="col-sm-3">
+                <figure class="module-thumb">
+                    <a href="">
+                        <div class="module-thumb-img">
+                            <img src="/img/test.jpg">
+                        </div>
+                        <figcaption class="module-thumb-caption">
+                            <span class="module-thumb-caption-header">CS4203</span>
+                            <br>
+                            <span class="module-thumb-caption-text">SOUND AND MUSIC COMPUTING</span>
+                        </figcaption>
+                    </a>
+                </figure>
+            </div>
+            <div class="col-sm-3">
+                <figure class="module-thumb">
+                    <a href="">
+                        <div class="module-thumb-img">
+                            <img src="/img/test.jpg">
+                        </div>
+                        <figcaption class="module-thumb-caption">
+                            <span class="module-thumb-caption-header">CS4203</span>
+                            <br>
+                            <span class="module-thumb-caption-text">SOUND AND MUSIC COMPUTING</span>
+                        </figcaption>
+                    </a>
+                </figure>
+            </div>
+            <div class="col-sm-3">
+                <figure class="module-thumb">
+                    <a href="">
+                        <div class="module-thumb-img">
+                            <img src="/img/test.jpg">
+                        </div>
+                        <figcaption class="module-thumb-caption">
+                            <span class="module-thumb-caption-header">CS4203</span>
+                            <br>
+                            <span class="module-thumb-caption-text">SOUND AND MUSIC COMPUTING</span>
+                        </figcaption>
+                    </a>
+                </figure>
+            </div>
+            <div class="col-sm-3">
+                <figure class="module-thumb">
+                    <a href="">
+                        <div class="module-thumb-img">
+                            <img src="/img/test.jpg">
+                        </div>
+                        <figcaption class="module-thumb-caption">
+                            <span class="module-thumb-caption-header">CS4203</span>
+                            <br>
+                            <span class="module-thumb-caption-text">SOUND AND MUSIC COMPUTING</span>
+                        </figcaption>
+                    </a>
+                </figure>
+            </div>
+        </div>
+        <br>
+        <a href="talks.php" class="btn btn-primary">See all modules</a>
+    </div>
+
+    <hr>
+
+    <!-- registration prompt -->
+    <div class="block--spaced">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h2 class="section__heading">Come On Down</h2>
+                <div class="u-center">
+                    <p>
+                        Buffet dinner will be provided to add registered attendees.<br>
+                        Confirm your spot by <a href="/index.php/register">registering</a>. It's free!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- publicity prompt -->
+    <div class="block--spaced">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h2 class="section__heading">Already Participating?</h2>
+                <div class="u-center">
+                    <p>
+                        Share the excitement with your friends and relatives!<br>
+                        Invite them down for a night of innovation, genius and enterprise
+                        <!-- fb like btn -->
+                        <div class="fb-like" data-href="http://steps.tk/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
