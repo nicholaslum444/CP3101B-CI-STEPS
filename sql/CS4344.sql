c66d6487e572b573559f79da64c308ae6c2bac3e
MunAw <jasontanma@gmail.com>
2015-04-13 20:38:38 +0800

    DB side changes: now our 4 users can login as if it is real and modify CP3101 STPES project. now added poster and video attribute to module added FYP to module added CS4344 information to it. fix null error in food pref

---
+++ sql/CS4344.sql
@@ -0,0 +1,154 @@
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Captain Hook ',7000,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','Captain Hook is a simplified version of DOTA\'s Pudge War.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110001B','JIN XIAOJIE','a@nus.edu.sg',60000000,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110001B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110001B','7000');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110002B',' MUHAMMAD FARHAN BIN ISMAIL','a@nus.edu.sg',60000001,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110002B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110002B','7000');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110003B',' PHAN SHI YU','a@nus.edu.sg',60000002,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110003B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110003B','7000');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110004B',' WU LONG ','a@nus.edu.sg',60000003,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110004B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110004B','7000');
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Elemental Frenzy ',7001,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','Elemental Frenzy is a 2D side-scrolling platform player-vs-player (PvP) battle arena, with a Deathmatch-style game mode.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110005B','CHANG YAN QIAN','a@nus.edu.sg',60000004,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110005B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110005B','7001');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110006B',' CHUE SAI HOU','a@nus.edu.sg',60000005,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110006B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110006B','7001');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110007B',' LIM YU DE','a@nus.edu.sg',60000006,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110007B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110007B','7001');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110008B',' YIP JIAJIE ','a@nus.edu.sg',60000007,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110008B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110008B','7001');
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Fighting Arena ',7002,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','Fighting Arena is a 2D multi-player fighting game.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110009B','CHEN ZEYU','a@nus.edu.sg',60000008,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110009B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110009B','7002');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110010B',' WU PEI','a@nus.edu.sg',60000009,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110010B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110010B','7002');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110011B',' WU WEILUN ','a@nus.edu.sg',60000010,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110011B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110011B','7002');
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Gravitas ',7003,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','Gravitas is a multiplayer 2d side-scrolling shooter game.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110012B','ANGELA HENG MIN YI','a@nus.edu.sg',60000011,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110012B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110012B','7003');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110013B',' CHEW YI XIU','a@nus.edu.sg',60000012,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110013B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110013B','7003');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110014B',' WONG WEI JIE BOB','a@nus.edu.sg',60000013,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110014B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110014B','7003');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110015B',' WONG ZHI QIAN SEBASTIAN ','a@nus.edu.sg',60000014,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110015B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110015B','7003');
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Nutty Ninja X ',7004,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','Nutty Ninjas X is a sequel to the hit game Nutty Ninjas, and it makes use of a portal mechanics to add a new twist to the original game.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110016B','NG ZHENG HAN','a@nus.edu.sg',60000015,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110016B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110016B','7004');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110017B',' NGUYEN TRUNG HIEU','a@nus.edu.sg',60000016,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110017B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110017B','7004');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110018B',' TAY YANG SHUN ','a@nus.edu.sg',60000017,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110018B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110018B','7004');
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Shape Survival Arena ',7005,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','SSA is a multiplayer, round-based dual-stick shooter game.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110019B','FOONG TUCK MENG DARREN','a@nus.edu.sg',60000018,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110019B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110019B','7005');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110020B',' SOURABH RAO','a@nus.edu.sg',60000019,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110020B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110020B','7005');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110021B',' VIGNESH LAKSHMI RAJAKUMAR','a@nus.edu.sg',60000020,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110021B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110021B','7005');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110022B',' ZENG QINGTAO ','a@nus.edu.sg',60000021,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110022B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110022B','7005');
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Sockick ',7006,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','Sockick is a multi-player soccer (aka footbal) game.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110023B','LI YAN','a@nus.edu.sg',60000022,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110023B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110023B','7006');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110024B',' LIU TUO','a@nus.edu.sg',60000023,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110024B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110024B','7006');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110025B',' QING CHENG','a@nus.edu.sg',60000024,2,1);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110025B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110025B','7006');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110026B',' YANG ZHIXING ','a@nus.edu.sg',60000025,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110026B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110026B','7006');
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Starvun ',7007,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','Multiplayer, competitive, pac-man, similar to Pacman Battle Royale.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110027B','DU YANXIAN','a@nus.edu.sg',60000026,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110027B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110027B','7007');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110028B',' EDRICK RUDY PUTRA','a@nus.edu.sg',60000027,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110028B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110028B','7007');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110029B',' LAU YEW KAI','a@nus.edu.sg',60000028,2,1);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110029B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110029B','7007');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110030B',' PRIYADHARSHINI TAMILARASAN ','a@nus.edu.sg',60000029,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110030B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110030B','7007');
+INSERT INTO `project` (`title`, `project_id`,`module_id`,`abstract`,`poster`,`video`) VALUES 
+('Step On You ',7008,'kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk','A simpler, Web-based, clone of Super Mario Wars.','http://media.tumblr.com/3f81fad6ef163759fff8077580d94752/tumblr_inline_nj4nxkE0C21qersu1.png','https://www.youtube.com/watch?v=MYZfjfkwRII');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110031B','CHOO CHENG MUN PAULINA','a@nus.edu.sg',60000030,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110031B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110031B','7008');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110032B',' LAI YONGQUAN','a@nus.edu.sg',60000031,2,1);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110032B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110032B','7008');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110033B',' NGUYEN TRONG SON','a@nus.edu.sg',60000032,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110033B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110033B','7008');
+INSERT INTO `user` (`user_id`,`name`,`email`,`contact`,`user_type`,`food_preference`) VALUES
+('A5110034B',' PHAM MINH VIET ','a@nus.edu.sg',60000033,3,2);
+INSERT INTO `enrolled` (`user_id`,`module_id`) VALUES ('A5110034B','kkkkkkkk-kkkk-kkkk-kkkk-kkkkkkkkkkkk');
+INSERT INTO `participate` (`user_id`,`project_id`) VALUES ('A5110034B','7008');
