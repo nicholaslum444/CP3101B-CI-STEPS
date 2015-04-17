0d7d614c2bec2577ecf4a8d3d35eac89d6f3d8c5
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-17 15:30:46 +0800

    Changed a bit on register pag

--- application/views/public/RegisterPage.php
+++ application/views/public/RegisterPage.php
@@ -15,7 +15,7 @@
         <div class="col-sm-6">
                 <h2 class="section__heading about-page-right">Registration</h2>
                 <div class="steps-poster-container about-page-left">
-                    <iframe src="//eventbrite.sg/tickets-external?eid=16153884704&amp;ref=etckt" frameborder="0" height="250" width="550" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+                    <iframe src="//eventbrite.sg/tickets-external?eid=16153884704&amp;ref=etckt" width="100%" height="400px" frameborder="0" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
                 </div>
         </div>
         <div class="col-sm-6">
@@ -36,7 +36,7 @@
         <div class="col-sm-6">
             <h2 class="section__heading about-page-right">Location</h2>
             <div class="steps-poster-container about-page-right">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3988.7991789190214!2d103.773846!3d1.295053!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9f0d02392be4dfbe!2sNational+University+of+Singapore+School+of+Computing!5e0!3m2!1sen!2ssg!4v1410769996005" width="400" height="300" frameborder="0" style="border:0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3988.7991789190214!2d103.773846!3d1.295053!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9f0d02392be4dfbe!2sNational+University+of+Singapore+School+of+Computing!5e0!3m2!1sen!2ssg!4v1410769996005"  frameborder="0" style="border:0"></iframe>
             </div>
         </div>
         <div class="col-sm-6">
@@ -44,10 +44,11 @@
             <div class="steps-poster-container about-page-left">
                 <iframe src="https://www.eventbrite.sg/countdown-widget?eid=16153884704" frameborder="0" height="223" width="195" marginheight="0" marginwidth="0" scrolling="no" allowtransparency="true"></iframe>
             </div>
-            <a href="https://www.facebook.com/events/737647543000395/" class="btn btn-info btn-large"><i class="icon-white icon-user"></i>We're on Facebook</a>  Help get the word out!
+            <a href="https://www.facebook.com/events/737647543000395/" class="btn btn-info btn-large"><i class="icon-white icon-user"></i>We're on Facebook</a><p>Help get the word out!</p>
+            <div class="fb-like" data-href="http://steps.tk/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
         </div>
     </div>
 
     <hr>
-    
+
 </div>
