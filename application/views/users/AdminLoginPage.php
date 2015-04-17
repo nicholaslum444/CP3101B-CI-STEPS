0edb22c0b62771f12cf92dd2b06b6a94ae566212
Sharmine <slxmlow@gmail.com>
2015-04-17 11:49:08 +0800

    Redesign admin login page

--- application/views/users/AdminLoginPage.php
+++ application/views/users/AdminLoginPage.php
@@ -1,35 +1,47 @@
 
-<!-- please style this page when there is time -->
-
-<div class="container site-container">
+<div class="site-container admin-loginPage-container">
   <div class="row">
-    <h3>Admin Login</h3>
-    <!-- form must POST the username and password to this page -->
-    <form class="form-horizontal" role="form" action="" method="POST">
-      <div class="form-group">
-        <label class="control-label col-sm-4" for="username">Admin Username:</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" id="username" name="username" placeholder="Admin Username">
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="control-label col-sm-4" for="password">Password</label>
-        <div class="col-sm-4">
-          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-        </div>
-      </div>
 
-      <!-- this chunk will only appear if the user key in his user/pass wrong the first time -->
-      <!-- may want to restyle this -->
-      <?php if($retry) { ?>
-        <p style="color:red;">Wrong username/password combination!</p>
-      <?php } ?>
+    <div class ="small-login-box col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1">
 
-      <div class="form-group">
-        <div class="col-sm-8">
-          <input type="submit" class="form-control" id="numProjects" placeholder="Password">
+      <form class="form" role="form" action="" method="POST">
+        
+        <div class="row">
+          <div class="form-group">
+            <label class="control-label col-md-3" for="username">Admin Username:</label>
+            <div class="col-md-8 col-sm-12">
+              <input type="text" class="form-control" id="username" name="username" placeholder="Admin Username">
+            </div>
+          </div>
         </div>
-      </div>
-    </form>
+
+        <div class="row">
+          <div class="form-group">
+            <label class="control-label col-md-3" for="password">Password:</label>
+            <div class="col-md-8 col-sm-12">
+              <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group" id="admin-login-btn-div">
+          <div class="col-md-4 col-md-offset-8 col-xs-12">
+            <input type="submit" class="form-control btn btn-secondary" id="numProjects" placeholder="Password">
+          </div>
+        </div>
+
+        <!-- this chunk will only appear if the user key in his user/pass wrong the first time -->
+        <!-- may want to restyle this -->
+        <?php 
+          if($retry) { 
+        ?>
+        <div id="admin-bad-login-msg">Wrong username/password combination!</div>
+        <?php } ?>
+
+      </form>
+    </div><!-- end of login box-->
+
+
+
   </div>
 </div>
